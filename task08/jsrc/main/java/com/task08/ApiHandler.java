b473054d30457450905a02924f528664edde6ce7
Amresh Ranjan <amresh_ranjan@epam.com>
2024-08-06 23:21:52 +0530

    Update openmeteo code

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -10,8 +10,10 @@ import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import org.epam.openapi.App;
 
 import java.io.IOException;
+
 
 @LambdaHandler(lambdaName = "api_handler",
         roleName = "api_handler-role",
@@ -20,8 +22,8 @@ import java.io.IOException;
 )
 @LambdaLayer(
         layerName = "sdk-layer",
-        libraries = {"lib/OpenMeteo.zip"},
-        artifactExtension = ArtifactExtension.ZIP
+        libraries = {"lib/open-meteo-api-1.0-SNAPSHOT.jar"},
+        artifactExtension = ArtifactExtension.JAR
 )
 @LambdaUrlConfig(
         authType = AuthType.NONE,
@@ -31,9 +33,7 @@ public class ApiHandler implements RequestHandler<Object, JsonObject> {
 
     public JsonObject handleRequest(Object request, Context context) {
 
-//        double latitude=request.get("latitude");
-//        double longitude= request.get("longitude");
-        OpenMeteo client = new OpenMeteo(50.4375, 30.5);
+        App client = new App(50.4375, 30.5);
         JsonObject weatherData = null;
         try {
             weatherData = client.getWeatherForecast();
