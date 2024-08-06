e7eb31a7b7485eb5ef0cab2f7dc353f5dd1135ec
Amresh Ranjan <amresh_ranjan@epam.com>
2024-08-06 22:28:28 +0530

    Update openmeteo code

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -6,12 +6,12 @@ import com.google.gson.JsonObject;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.io.IOException;
-import java.util.Map;
 
 @LambdaHandler(lambdaName = "api_handler",
         roleName = "api_handler-role",
@@ -19,21 +19,22 @@ import java.util.Map;
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaLayer(
-        layerName = "sdk-layer"
+        layerName = "sdk-layer",
+        libraries = {"lib/OpenMeteo.zip"},
+        artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
         authType = AuthType.NONE,
         invokeMode = InvokeMode.BUFFERED
 )
-public class ApiHandler implements RequestHandler<Map<String,Double>, JsonObject> {
+public class ApiHandler implements RequestHandler<Object, JsonObject> {
 
-    public JsonObject handleRequest(Map<String,Double> request, Context context) {
+    public JsonObject handleRequest(Object request, Context context) {
 
-        double latitude=request.get("latitude");
-        double longitude= request.get("longitude");
-        OpenMeteo client = new OpenMeteo(latitude, longitude);
-
-        JsonObject weatherData=null;
+//        double latitude=request.get("latitude");
+//        double longitude= request.get("longitude");
+        OpenMeteo client = new OpenMeteo(50.4375, 30.5);
+        JsonObject weatherData = null;
         try {
             weatherData = client.getWeatherForecast();
         } catch (IOException e) {
