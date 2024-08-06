7c8a4dda37a93829140afdc0ca6ec6c427de6efc
Amresh Ranjan <amresh_ranjan@epam.com>
2024-08-07 01:12:29 +0530

    Add updated code

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -6,7 +6,9 @@ import com.google.gson.JsonObject;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
@@ -18,20 +20,24 @@ import java.io.IOException;
 @LambdaHandler(lambdaName = "api_handler",
         roleName = "api_handler-role",
         layers = "sdk-layer",
+        runtime = DeploymentRuntime.JAVA11,
+        architecture = Architecture.ARM64,
         logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaLayer(
         layerName = "sdk-layer",
         libraries = {"lib/open-meteo-api-1.0-SNAPSHOT.jar"},
-        artifactExtension = ArtifactExtension.JAR
+        runtime = DeploymentRuntime.JAVA11,
+        architectures = {Architecture.ARM64},
+        artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
         authType = AuthType.NONE,
         invokeMode = InvokeMode.BUFFERED
 )
-public class ApiHandler implements RequestHandler<Object, JsonObject> {
+public class ApiHandler implements RequestHandler<Object, String> {
 
-    public JsonObject handleRequest(Object request, Context context) {
+    public String handleRequest(Object request, Context context) {
 
         App client = new App(50.4375, 30.5);
         JsonObject weatherData = null;
@@ -41,6 +47,6 @@ public class ApiHandler implements RequestHandler<Object, JsonObject> {
             throw new RuntimeException(e);
         }
 
-        return weatherData;
+        return weatherData.toString();
     }
 }
